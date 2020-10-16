a158d9d61e4966455013d7f5272e5eb8d0d80ade
Daniel Kim <daniel.kim526@gmail.com>
2020-10-15 23:34:06 -0500

    95% complete on making scrollOnReveal function more efficient. If statements added to turn off event listeners whenelements are revealed keeping function usage to a min.

--- app/assets/scripts/modules/RevealOnScroll.js
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -1,30 +1,45 @@
-import { functions } from "lodash";
+import throttle from 'lodash/throttle';
 
 class RevealOnScroll {
     constructor() {
         this.itemsToReveal = document.querySelectorAll(".feature-item");
         this.hideInitiallly();
+        this.scrollThrottle = throttle(this.calcCaller, 200).bind(this);
         this.events();
     }
 
     events() {
-        window.addEventListener("scroll", () => {
+        window.addEventListener("scroll", this.scrollThrottle)
+    }
+
+    calcCaller() {
+        console.log("Scroll function ran.")
             this.itemsToReveal.forEach(el => {
-                this.calculateIfScrolledTo(el)
-            })
-        }) 
+                if (el.isRevealed == false) {
+                    this.calculateIfScrolledTo(el);
+                }
+            });
     }
 
     calculateIfScrolledTo(el) {
-        //console.log(el.getBoundingClientRect().y);
-        let scrollPercent = (el.getBoundingClientRect().y / window.innerHeight) * 100;
+        //console.log(el.getBoundingClientRect().top);
+        console.log("Element was calculated.");
+        let scrollPercent = (el.getBoundingClientRect().top / window.innerHeight) * 100;
         if (scrollPercent < 75) {
-            el.classList.add("reveal-item--is-visible")
+            el.classList.add("reveal-item--is-visible");
+            el.isRevealed = true;
+            if (el.isLastItem) {
+                window.removeEventListener("scroll", this.scrollThrottle);
+            }
         }
     }
 
     hideInitiallly() {
-        this.itemsToReveal.forEach(el => el.classList.add("reveal-item"));
+        this.itemsToReveal.forEach(el => {
+            el.classList.add("reveal-item");
+            el.isRevealed = false;
+        });
+        this.itemsToReveal[this.itemsToReveal.length - 1].isLastItem = true;
     }
 }
 
