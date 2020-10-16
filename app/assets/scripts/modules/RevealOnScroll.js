ca645284710e852c5aef908aba368f4d4df7e271
Daniel Kim <daniel.kim526@gmail.com>
2020-10-16 00:16:14 -0500

    Reveal on scroll is more efficient and now applied to testimonials.

--- app/assets/scripts/modules/RevealOnScroll.js
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -1,15 +1,22 @@
 import throttle from 'lodash/throttle';
+import debounce from 'lodash/debounce';
 
 class RevealOnScroll {
-    constructor() {
-        this.itemsToReveal = document.querySelectorAll(".feature-item");
+    constructor(els, thresholdPercent) {
+        this.thresholdPercent = thresholdPercent;
+        this.itemsToReveal = els;
+        this.browserHeight = window.innerHeight;
         this.hideInitiallly();
         this.scrollThrottle = throttle(this.calcCaller, 200).bind(this);
         this.events();
     }
 
     events() {
-        window.addEventListener("scroll", this.scrollThrottle)
+        window.addEventListener("scroll", this.scrollThrottle);
+        window.addEventListener("resize", debounce(() => {
+            console.log("Resize just ran.");
+            this.browserHeight = window.innerHeight;
+        }, 333));
     }
 
     calcCaller() {
@@ -22,15 +29,17 @@ class RevealOnScroll {
     }
 
     calculateIfScrolledTo(el) {
-        //console.log(el.getBoundingClientRect().top);
+        if (window.scrollY + this.browserHeight > el.offsetTop) {
+            //console.log(el.getBoundingClientRect().top);
         console.log("Element was calculated.");
-        let scrollPercent = (el.getBoundingClientRect().top / window.innerHeight) * 100;
-        if (scrollPercent < 75) {
+        let scrollPercent = (el.getBoundingClientRect().top / this.browserHeight) * 100;
+        if (scrollPercent < this.thresholdPercent) {
             el.classList.add("reveal-item--is-visible");
             el.isRevealed = true;
             if (el.isLastItem) {
                 window.removeEventListener("scroll", this.scrollThrottle);
             }
+        }
         }
     }
 
