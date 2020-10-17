f6f6aa2c442a609d1ece5efeb8262f57e6791051
Daniel Kim <daniel.kim526@gmail.com>
2020-10-16 19:35:22 -0500

    lazyloading added

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,4 +1,5 @@
 import '../styles/styles.css';
+import 'lazysizes';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
@@ -18,8 +19,8 @@ document.querySelectorAll(".open-modal").forEach(el => {
         e.preventDefault();
         if (typeof modal == "undefined") {
             import(/* webpackChunkName: "modal" */'./modules/Modal').then(x => {
-                modal = new x.default()
-                setTimeout(() => modal.openTheModal(), 20)
+                modal = new x.default();
+                setTimeout(() => modal.openTheModal(), 20);
              }).catch(() => console.log("There was a problem."))
         } else {
             modal.openTheModal();
